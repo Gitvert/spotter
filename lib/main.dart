bf99c9478831d4fe3e65ca7fe5d420f7a1a347a8
Unknown <jesper@piktiv.se>
2019-07-13 11:26:26 +0200

    Add a new start page with a button to go to the logging page

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,35 @@ import 'widgets/WeightWidget.dart';
 
 void main() => runApp(MaterialApp(
     title: 'spotter',
-    home: WeightWidget()
+    home: HomePage()
 ));
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[700],
+      appBar: AppBar(
+        title: Row(
+            children: [
+              Text('spotter'),
+              Icon(Icons.fitness_center),
+            ]
+        ),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center (
+        child: FlatButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => WeightWidget()));
+          },
+          child: Text('LOG SET')
+        )
+      )
+    );
+  }
+}
 
 /*class MyApp extends StatelessWidget {
   @override
