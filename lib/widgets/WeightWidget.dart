44ea1356fdfcf3225e7550d72af10778275bd251
Unknown <jesper@piktiv.se>
2019-07-13 10:59:31 +0200

    Remove unnecessary Containers

--- lib/widgets/WeightWidget.dart
+++ lib/widgets/WeightWidget.dart
@@ -38,35 +38,31 @@ Widget _getAdjustWidget(String text, int number) {
       padding: const EdgeInsets.only(top: 5, bottom: 15),
       child: Column(
         children: [
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  text,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16
-                  )
-                ),
-              ]
-            )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16
+                )
+              ),
+            ]
           ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _getIconWidget(Icons.remove_circle_outline),
-                Text(
-                  number.toString(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40
-                  )
-                ),
-                _getIconWidget(Icons.add_circle_outline)
-              ]
-            )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _getIconWidget(Icons.remove_circle_outline),
+              Text(
+                number.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40
+                )
+              ),
+              _getIconWidget(Icons.add_circle_outline)
+            ]
           )
         ],
       )
